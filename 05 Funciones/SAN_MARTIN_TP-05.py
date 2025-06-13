68b139e568a7c2e77f5ff1d6d9be713f031716e2
Michel San Martin <mikeemmanuel91@gmail.com>
2025-06-13 14:35:12 -0300

    parcial delivery

---
+++ 05 Funciones/SAN_MARTIN_TP-05.py
@@ -0,0 +1,8 @@
+import utils
+# A modo aclaratorio, se tomara este archivo como Main o principal.
+
+#Actividad 1)
+utils.imprimir_hola_mundo()
+
+#Actividad 2)
+utils.saludar_usuario(input("Por favor, ingrese su nombre: "))
--- 05 Funciones/utils.py
+++ 05 Funciones/utils.py
@@ -1,2 +1,43 @@
-def print_message(message):
-    print(message)
+import math
+
+# Funciones auxiliares
+def saludar(nombre = "Mundo"):
+    saludo = f"Hola {nombre}!"
+    print(saludo)
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+
+def solicitar_input_usuario(campo):
+    valor = input(f"Por favor ingrese {campo}:")   
+    return valor
+
+def solicitar_numero_usuario(numero):
+    valor = int(solicitar_input_usuario(numero))
+    return valor
+
+# Funciones actividades
+# Actividad 1)
+def imprimir_hola_mundo():
+    return saludar()
+
+# Actividad 2)
+def saludar_usuario(nombre):
+    return saludar(nombre)
+
+# Actividad 3)
+def imprimir_informacion_personal(nombre, apellido, edad, residencia):
+    mensaje_informacion = f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}"
+    print(mensaje_informacion)
+
+# Actividad 4)
+def calcular_area_circulo(radio):
+    calcular_perimetro_circulo(radio)
+    area_circulo = (math.pi)*(radio ** 2)
+
+    imprimir_mensaje(f"El area del circulo es:{area_circulo}")
+
+def calcular_perimetro_circulo(radio):
+    perimetro_circulo = 2*(math.pi)*radio
+    imprimir_mensaje(f"El perimetro del circulo es: {perimetro_circulo}")
+    #return(perimetro_circulo)
