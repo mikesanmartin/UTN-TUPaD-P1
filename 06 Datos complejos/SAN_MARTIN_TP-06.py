d70999c9b97ac4b50a67210d42ae497925a46bee
Michel San Martin <mikeemmanuel91@gmail.com>
2025-06-14 13:33:24 -0300

    partial delivery tp 6

---
+++ 06 Datos complejos/SAN_MARTIN_TP-06.py
@@ -0,0 +1,28 @@
+# Actividad 1
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+1450}
+
+# Se agregan siguientes frutas y precios
+precios_frutas['Naranja'] = 1200
+precios_frutas['Manzana'] = 1500
+precios_frutas['Pera'] = 2300
+
+# Verifico que se hayan agregado al diccionario
+print(precios_frutas)
+
+# Actividad 2
+# Se realizan modificaciones
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melon'] = 2800 
+
+# Verifico que se haya modificado el diccionario
+print(precios_frutas)
+
+# Actividad 3
+lista_solo_frutas = [precios_frutas.keys()]
+
+# Verifico nueva lista
+print(lista_solo_frutas)
+
+# Actividad 4
